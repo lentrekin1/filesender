61cf9d8814571d0fbdbd525f8ad031f19418db29
lance <lancee478@gmail.com>
2021-05-24 20:18:17 -0700

    reformat code

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template, request, redirect, flash, send_file
 import time
 from io import BytesIO
+
+from flask import Flask, render_template, request, redirect, flash, send_file
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'rehegrjvfdn.weroro4h3dhio2ue3wfhergvdfkljcsk./jwqe3iru4t5gg'
@@ -11,6 +12,7 @@ payload = None
 payload_name = None
 
 footer = '<br><br><button type="button" onclick="location.href=\'/\'" class="btn btn-primary">Back</button>'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
@@ -25,15 +27,16 @@ def hello_world():
         payload = request.files['file'].read()
         payload_name = request.files['file'].filename
         upload_time = time.time()
-        flash(f'File upload successful, available for {timeout/60} mins')
+        flash(f'File upload successful, available for {timeout / 60} mins')
     elif request.form['msg']:
         payload = request.form['msg']
         upload_time = time.time()
-        flash(f'Msg upload successful, available for {timeout/60} mins')
+        flash(f'Msg upload successful, available for {timeout / 60} mins')
     else:
         flash('Enter a file or msg')
 
     return redirect('/')
+
 
 @app.route('/recv')
 def send():
@@ -50,6 +53,7 @@ def send():
         else:
             flash('No file or msg found')
             return redirect('/')
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
